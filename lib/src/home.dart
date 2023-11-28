f6851adbe0f3d2556fb4295b613c17762d993a02
Robertas Skiauteris <Shalomka@users.noreply.github.com>
2023-11-28 16:16:48 +0100

    code cleanup

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,35 +1,32 @@
+import 'package:delayed_display/delayed_display.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kyla_home_budget/src/features/core/ui/error_page.dart';
 import 'package:kyla_home_budget/src/features/core/ui/loader_view.dart';
+import 'package:kyla_home_budget/src/features/core/ui/profile_scaffold.dart';
 import 'package:kyla_home_budget/src/features/expenses/providers/expenses_provider.dart';
 import 'package:kyla_home_budget/src/features/expenses/ui/expense_card.dart';
-import 'package:kyla_home_budget/src/features/in_app_messages/ui/in_app_message_view.dart';
 
 class Home extends ConsumerWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final expenseLoader = ref.watch(expenseProvider);
-    return Scaffold(
+    final expenseLoader = ref.watch(expensesProvider);
+    return ProfileScaffold(
       body: expenseLoader.when(
         data: (expenses) {
-          return SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: InAppMessageView(
-                message: 'Hello World',
-                onDismiss: () {},
-                onAction: () {},
-              ),
-            ),
-          );
           //TODO: add an empty state
-          return ListView.builder(
+          return ListView.separated(
+            padding: const EdgeInsets.only(top: 14),
+            separatorBuilder: (context, index) => const SizedBox(
+              height: 8,
+            ),
             physics: const BouncingScrollPhysics(),
             itemBuilder: (context, index) {
-              return ExpenseCard(expenses[index]);
+              return DelayedDisplay(
+                  delay: Duration(milliseconds: index < 3 ? 100 * index : 300),
+                  child: ExpenseCard(expenses[index]));
             },
             itemCount: expenses.length,
           );
